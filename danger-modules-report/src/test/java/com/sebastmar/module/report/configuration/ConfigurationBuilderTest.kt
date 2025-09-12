eba325758238e2e34f2cbff4c370a8ecaa725a0c
Sébastien Martin <sebast.mar@gmail.com>
2025-09-12 09:56:22 +0200

    Add code coverage badge to README (#8)
    Add coverage badge

--- danger-modules-report/src/test/java/com/sebastmar/module/report/configuration/ConfigurationBuilderTest.kt
+++ danger-modules-report/src/test/java/com/sebastmar/module/report/configuration/ConfigurationBuilderTest.kt
@@ -22,6 +22,21 @@ internal class ConfigurationBuilderTest {
     }
 
     @Test
+    fun `verify the default configuration`() {
+        val givenDefaultLinkify = true
+        val givenDefaultShowCircle = true
+        val givenDefaultShowLine = true
+        val givenDefaultSkipKeyword = "module-no-report"
+        val givenDefaultReportStrings = ReportStrings()
+
+        assertEquals(givenDefaultLinkify, builder.linkifyFiles)
+        assertEquals(givenDefaultShowCircle, builder.showCircleIndicators)
+        assertEquals(givenDefaultShowLine, builder.showLineIndicators)
+        assertEquals(givenDefaultSkipKeyword, builder.skipReportKeyword)
+        assertEquals(givenDefaultReportStrings, builder.reportStrings)
+    }
+
+    @Test
     fun `verify it builds the default configuration`() {
         val givenDefaultLinkify = true
         val givenDefaultShowCircle = true
